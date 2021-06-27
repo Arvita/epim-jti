473c75e4b2ab3b0b400f19c4e64df958f9cadfd4
Arvita Agus kurniasari <aqwuarvitha@gmail.com>
2021-06-27 16:02:13 +0700

    Update UserController.php

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User,
-    Yajra\DataTables\DataTables,
+    DataTables,
     Validator,
     Illuminate\Support\Facades\Crypt;
 use Illuminate\Http\Request;
