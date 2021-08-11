6a0e894d44b14a0e62f6b5bb85cd98777bd4766c
CodeVectorizer <lukmanafandi10@gmail.com>
2021-08-11 09:57:18 +0700

    edit modal

--- resources/views/admin/pages/expo/index.blade.php
+++ resources/views/admin/pages/expo/index.blade.php
@@ -193,7 +193,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Detail Produk</h5> <button type="button" class="close" data-dismiss="modal"
-                        aria-label="Close"> <span aria-hidden="true">Ãƒâ€”</span> </button>
+                        aria-label="Close"> <span aria-hidden="true">-</span> </button>
                 </div>
                 <div class="modal-body">
                     <div class="container">
